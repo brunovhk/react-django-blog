3e3530bfccd324050d55632f76d985ebf001d8e8
Bruno Vieira <brunorick11@gmail.com>
2025-05-15 17:59:33 -0300

    feat: add CORS support; include corsheaders in installed apps and middleware, and update requirements

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,12 +45,14 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "corsheaders",
     "users",
     "posts",
     "comments",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,6 +61,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+# Development only
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "config.urls"
 
