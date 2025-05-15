3d56536c5df4d5e791608e967f139f612cf94dbb
Bruno Vieira <brunorick11@gmail.com>
2025-05-15 13:42:19 -0300

    feat: update settings.py to use dj-database-url for database configuration and add required packages to requirements.txt; modify docker-compose.yml to include migration command

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        # ssl_require=True
+    )
 }
 
 
