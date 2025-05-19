16227e845827a61950cc6ffefdbf2d9ef26b5f9f
Bruno Vieira <brunorick11@gmail.com>
2025-05-18 21:18:28 -0300

    fix: update CORS and CSRF settings to use environment variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -68,7 +68,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 # Development only
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="http://localhost:8000").split(",")
+
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="http://localhost:8000").split(",")
 
 ROOT_URLCONF = "config.urls"
 
